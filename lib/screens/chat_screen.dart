4d8976077272fe8d2b366377d13b84824d0cc056
UjwalAgrawal <uvagrawal123@gmail.com>
2021-02-05 10:26:38 +0530

    getting started with firebase

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chats'),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) => Container(
+          padding: EdgeInsets.all(8),
+          child: Text('This works!'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('/chats/GfjeKSQn4vVbzSGGb1d9/messages')
+              .snapshots()
+              .listen((event) {
+            event.docs.forEach((element) {
+              print(element['text']);
+            });
+          });
+        },
+      ),
+    );
+  }
+}
