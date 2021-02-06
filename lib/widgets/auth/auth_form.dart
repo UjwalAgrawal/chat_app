558c7f8260cfc34878e7b3ef483ef114cd1bde13
UjwalAgrawal <uvagrawal123@gmail.com>
2021-02-06 16:43:28 +0530

    authentication form creation

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
 
 class AuthForm extends StatefulWidget {
   @override
@@ -6,6 +7,24 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+  var _userEmail = '';
+  var _userName = '';
+  var _userPassword = '';
+  bool _showPassword = false;
+
+  void _trySubmit() {
+    FocusScope.of(context).unfocus();
+    final isValid = _formKey.currentState.validate();
+    if (isValid) {
+      _formKey.currentState.save();
+      print(_userEmail);
+      print(_userName);
+      print(_userPassword);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -15,36 +34,88 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: EdgeInsets.all(16),
             child: Form(
+                key: _formKey,
                 child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: "Email Address",
-                  ),
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Username",
-                  ),
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                  ),
-                  obscureText: true,
-                ),
-                SizedBox(
-                  height: 12,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text("Login"),
-                ),
-                TextButton(onPressed: () {}, child: Text("Create new account!"))
-              ],
-            )),
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextFormField(
+                      key: ValueKey('email'),
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: "Email Address",
+                      ),
+                      // autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (value) {
+                        bool isValid = EmailValidator.validate(value);
+                        if (!isValid)
+                          return "Please provide a valid email address.";
+                        return null;
+                      },
+                      onSaved: (newValue) {
+                        _userEmail = newValue;
+                      },
+                    ),
+                    if (!_isLogin)
+                      TextFormField(
+                        key: ValueKey('username'),
+                        validator: (value) {
+                          if (value.isEmpty || value.length < 4) {
+                            return "Please enter at least 4 characters.";
+                          }
+                          return null;
+                        },
+                        onSaved: (newValue) {
+                          _userName = newValue;
+                        },
+                        decoration: InputDecoration(
+                          labelText: "Username",
+                        ),
+                      ),
+                    TextFormField(
+                      key: ValueKey('password'),
+                      // autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (value) {
+                        if (value.isEmpty || value.length < 7) {
+                          return "Password must be at least 7 characters long.";
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) {
+                        _userPassword = newValue;
+                      },
+                      obscureText: !_showPassword,
+                      decoration: InputDecoration(
+                          labelText: "Password",
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              Icons.remove_red_eye,
+                              color: _showPassword
+                                  ? Theme.of(context).primaryColor
+                                  : Colors.grey,
+                            ),
+                            onPressed: () {
+                              setState(() => _showPassword = !_showPassword);
+                            },
+                          )),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    ElevatedButton(
+                      onPressed: _trySubmit,
+                      child: Text(_isLogin ? "Login" : "Signup"),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          _isLogin = !_isLogin;
+                        });
+                      },
+                      child: Text(
+                          _isLogin ? "Create new account!" : "Already a user?"),
+                    )
+                  ],
+                )),
           ),
         ),
       ),
