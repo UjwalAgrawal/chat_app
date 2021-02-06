741fbbc811949787faf7dc14fefea9c2043e1b51
UjwalAgrawal <uvagrawal123@gmail.com>
2021-02-07 00:19:31 +0530

    authentication and logout

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -7,6 +8,36 @@ class ChatScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Chats'),
+        actions: [
+          DropdownButton(
+            items: [
+              DropdownMenuItem(
+                child: Container(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.exit_to_app),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text('Logout'),
+                    ],
+                  ),
+                ),
+                value: 'logout',
+              ),
+            ],
+            onChanged: (itemIdentifier) {
+              if (itemIdentifier == 'logout') {
+                FirebaseAuth.instance.signOut();
+              }
+            },
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
+          )
+        ],
       ),
       body: StreamBuilder(
         stream: FirebaseFirestore.instance
