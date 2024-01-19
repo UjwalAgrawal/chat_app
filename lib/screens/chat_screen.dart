84929b5aff1dd42924dbe4f97d1100b30591207a
UjwalAgrawal <uvagrawal123@gmail.com>
2024-01-19 11:29:02 +0530

    update pubspec

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,9 +1,43 @@
 import 'package:chat_app/widgets/chat/messages.dart';
 import 'package:chat_app/widgets/chat/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    final fbm = FirebaseMessaging.instance;
+    fbm.requestPermission();
+    FirebaseMessaging.onMessage.listen((element) {
+      print(element);
+      return;
+    });
+    FirebaseMessaging.onBackgroundMessage(
+        (message) => _backgroundMessageHandler(message));
+
+    // FirebaseMessaging.onMessageOpenedApp.any((element) {
+    //   print(element);
+    //   return;
+    // });
+    // FirebaseMessaging.onBackgroundMessage((message) {
+    //   print("this is a background mssage: $message");
+    //   return;
+    // });
+
+    super.initState();
+  }
+
+  Future<void> _backgroundMessageHandler(message) {
+    print(message.data);
+    // return;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
